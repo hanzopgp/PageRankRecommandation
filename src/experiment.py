24cf3416e19cba525589540a3af0c15bda3807e5
hanzo <enzodurandcontact@gmail.com>
2022-04-29 17:08:44 +0200

    script for experiment

--- src/experiment.py
+++ src/experiment.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import pprint
 
 
 watched_dict = {
@@ -27,35 +28,35 @@ unwatched_dict = {
 }
 
 score = 0
-experiment_dataset = np.array([[watched_dict, top_reco_dict, unwatched_dict]])
-print(experiment_dataset)
-size = len(experiment_dataset) 
+experiment_dataset = [[watched_dict, top_reco_dict, unwatched_dict]]
+size = len(experiment_dataset)
+pp = pprint.PrettyPrinter(depth=4, sort_dicts=False) 
 
 for i, values in enumerate(experiment_dataset):
 	watched_dict, top_reco_dict, unwatched_dict = values
-	print("---------------------- User", i+1, "/", size+1 ,"----------------------")
-	print("--> List of film seen by user")
-	print(watched_dict.values())
-	print("--> Choose 5 films you would recommand for that user in the following list :")
+	print("---------------------- User", i+1, "/", size ,"----------------------")
+	print("\n--> List of film seen by user")
+	print(list(watched_dict.values()))
+	print("\n--> Choose 5 films you would recommand for that user in the following list :")
 	tmp = dict(top_reco_dict, **unwatched_dict)
 	keys = list(dict(top_reco_dict, **unwatched_dict).keys())
 	random.shuffle(keys)
 	shuffled_dict = dict()
 	for key in keys:
 		shuffled_dict.update({key: tmp[key]})
-	print(shuffled_dict)
-	print("--> Your choice must be this format : 145/323/447/565/23")
+	pp.pprint(shuffled_dict)
+	print("\n--> Your choice must be this format : 145/323/447/565/23")
 	while True:
 		try:
 			help_choice = input().split("/")
 			assert len(help_choice) == 5
 			assert [help_choice[i] in shuffled_dict.keys() for i in range(len(help_choice))] == [True] * 5
 		except:
-			print("-> Please use the correct format, you must give 5 answers only, chosen in the previous list.")
+			print("\n--> Please use the correct format, you must give 5 answers only, chosen in the previous list.")
 			continue
 		else:
 			break
 	for choice in help_choice:
 		if choice in top_reco_dict.keys():
 			score += 1
-print("--> Please return this score :", score/(size*5))
+print("\n--> Please return this score :", score/(size*5))
